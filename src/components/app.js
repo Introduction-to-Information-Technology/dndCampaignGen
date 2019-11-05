aaf2f7325ec8224f6213e9bf7c08d42c59b0d6aa
JRPubs <contact@storymedic.com>
2019-11-05 23:52:13 +1100

    Delete navbar.

--- src/components/app.js
+++ src/components/app.js
@@ -1,17 +1,13 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Navbar from './Navbar/navbar';
+
 import Home from './Home/home';
 
 
 class App extends Component {
   render() {
     return (
-      <Router>
-          <Navbar />
-            <Route exact path="/" component={Home} />
-
-      </Router>
+     <Home />
     );
   }
 }
